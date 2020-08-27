84707a487e9f119fc504fcdeb6befc33aa1c4bbc
Misaki Ata <misakiata209625@gamil.com>
2020-08-28 00:55:48 +0900

    ata

--- static/js/common.js
+++ static/js/common.js
@@ -4,13 +4,7 @@ $(function(){
             document.getElementById("cs").href = "../static/css/white_css/base.css"; 
             document.getElementById("top_css").href = "../static/css/white_css/top_white.css";
             document.getElementById('img').src= "../static/image/top/kikyu.jpg";
-            // document.getElementById("content_css").href = "../static/css/white_css/content1.css";   
-        });
-        $('.btn_white').on('click',function changecontentCss(){
-            // document.getElementById("cs").href = "../static/css/white_css/base.css"; 
-            // document.getElementById("top_css").href = "../static/css/white_css/top_white.css";
-            // document.getElementById('img').src= "../static/image/top/kikyu.jpg";
             document.getElementById("content_css").href = "../static/css/white_css/content1.css";   
         });
-
+        
 });
